8104fca72b658020b34d49bc71de7ffa110702c6
Manzi <asifiwemanzi@gmail.com>
2021-11-16 16:17:17 +0200

    check

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import django_heroku
+# import django_heroku
 from pathlib import Path
 import os
 
@@ -162,4 +162,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
